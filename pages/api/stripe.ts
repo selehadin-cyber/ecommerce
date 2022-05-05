6215f29d560e2f4bcd11dc5a3a7d3ef11a025d32
selehadin-cyber <sele4416@gmail.com>
2022-05-05 22:08:20 +0300

    stripe file added

---
+++ pages/api/stripe.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    try {
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: "{{PRICE_ID}}",
+            quantity: 1,
+          },
+        ],
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      };
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+      res.redirect(303, session.url);
+    } catch (err: any) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}

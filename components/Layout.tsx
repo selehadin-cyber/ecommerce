6c7c442484a71daecef226e9669bce8e9de383de
selehadin-cyber <sele4416@gmail.com>
2022-05-07 21:34:53 +0300

    sanity meta

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,7 +17,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <meta name="description" property="og:description" content="ihsan store is where you can find the best electronics" />
         <meta property="og:type" content="video.movie" />
         <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
-        <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
+        <meta property="og:image" content="https://cdn.sanity.io/images/ta8yo7gl/production/a64b345016e96adfb8849af5521c8e0ecfe8f027-555x555.webp" />
 
       </head>
       <header>

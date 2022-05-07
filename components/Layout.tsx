c7eb8617b47b8c0ec9e04b470bf74895a51a7067
selehadin-cyber <sele4416@gmail.com>
2022-05-07 18:52:25 +0300

    SEO optimizations

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,7 +11,13 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="layout">
       <head>
-        <title>Phone and PC accesories</title>
+        <title>Ihsan store</title>
+        <meta property="og:type" content="website" />
+        <meta name="description" content="ihsan store is where you can find the best electronics" />
+        <meta property="og:title" content="Ihsan store" />
+        <meta property="og:description" content="ihsan store is where you can find the best electronics" />
+        <meta property="og:image" content="https://vercel.com/api/screenshot?dark=1&deploymentId=dpl_38BnNTkDFVZKwcSv7J3AJBi71sRv&withStatus=true" />
+        <meta name="twitter:image" content="https://vercel.com/api/screenshot?dark=1&deploymentId=dpl_38BnNTkDFVZKwcSv7J3AJBi71sRv&withStatus=true" />
       </head>
       <header>
         <Navbar />

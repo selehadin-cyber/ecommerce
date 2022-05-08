2eba08038073db9016021c4c13f711381f1c2d77
selehadin-cyber <sele4416@gmail.com>
2022-05-08 12:08:39 +0300

    removed _app.tsx head link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,33 +8,12 @@ import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-    <Head>
-    <title>Ihsan store</title>
-        <meta property="og:type" content="website" />
-        <meta
-          name="description"
-          property="og:description"
-          content="ihsan store is where you can find the best electronics"
-        />
-        <meta property="og:type" content="video.movie" />
-        <meta
-          property="og:url"
-          content="https://www.imdb.com/title/tt0117500/"
-        />
-        <meta
-          name="image"
-          property="og:image"
-          content="https://github.com/selehadin-cyber/ecommerce/raw/main/components/screenshot.png"
-        />
-    </Head>
-      <StateContext>
-        <Layout>
-          <Toaster />
-          <Component {...pageProps} />
-        </Layout>
-      </StateContext>
-    </>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
 
